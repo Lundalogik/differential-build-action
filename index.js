5727369de5012f27bba61c62002b5ae81e613bcd
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-05-28 13:53:17 +0200

    feat: first version, hardcoded for lime-webclient

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
-try {
-    const changedFiles = core.getInput('changed-files');
-    console.log(`Changed files: ${changedFiles}`);
-    core.setOutput("run_python", 'false');
-    core.setOutput("run_admin", 'false');
-    core.setOutput("run_import", 'false');
-    core.setOutput("run_webclient", 'false');
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+async function run() {
+    const token = core.getInput('githubToken');
+    const octokit = new github.GitHub(token);
+
+    const { data: files } = await octokit.pulls.listFiles({
+        owner: github.context.payload.repository.owner.login,
+        repo: github.context.payload.repository.name,
+        pull_number: github.context.payload.pull_request.number
+    });
+
+    const filenames = files.map(file => file.filename);
+
+    // If the workflows were updated, run everything!
+    const workflowsUpdated = !!filenames.find(file => file.startsWith('.github/workflows'));
+
+    const run_python = workflowsUpdated || !!filenames.find(file => !file.startsWith('frontend/'));
+    const run_admin = workflowsUpdated || !!filenames.find(file => file.startsWith('frontend/admin/'));
+    const run_import = workflowsUpdated || !!filenames.find(file => file.startsWith('frontend/import/'));
+    const run_webclient = workflowsUpdated || !!filenames.find(file => file.startsWith('frontend/webclient/'));
+
+    core.setOutput('run_python', `${run_python}`);
+    core.setOutput('run_admin', `${run_admin}`);
+    core.setOutput('run_import', `${run_import}`);
+    core.setOutput('run_webclient', `${run_webclient}`);
 }
+
+run();
